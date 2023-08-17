40334948a7b840a4ad1f709d3b27f5b49f176c3c
Jean Pütz <jean-a.puetz@t-online.de>
2023-08-17 21:20:16 +0200

    Changed the favicon to have rounded courners

--- src/frontend/src/layout/Login.tsx
+++ src/frontend/src/layout/Login.tsx
@@ -1,23 +1,27 @@
-import { Box, Button, Stack, TextField, Typography } from "@mui/material";
+import { Box, Button, Stack, TextField, Typography, autocompleteClasses } from "@mui/material";
 
 export function Login() {
 
+    const getUserCredentials = () => {
+
+    }
+
     return(
         <Box >
-            <Stack direction="column">
-                <Typography className="mainMenu" textAlign='center' sx={{backgroundColor: "transparent"}}>
+            <Stack className="login" direction="column" spacing={"8px"} sx={{maxWidth: '400px', mt: '100px', justifyContent: 'center'}}>
+                <Typography textAlign="center">
                     Welcome to Guess The Number <br/> by MobDevOne
                 </Typography>
-                <TextField variant="outlined" sx={{maxWidth: '300px'}}>
+                <TextField label="Username" variant="outlined">
                     Username
                 </TextField>
-                <TextField variant="outlined" sx={{maxWidth: '300px'}}>
+                <TextField label="Password" variant="outlined">
                     Password
                 </TextField>
-                <Button variant="contained" sx={{maxWidth: '300px'}}>
+                <Button variant="contained" onClick={getUserCredentials}>
                     Log in
                 </Button>
-                <Button variant="text" sx={{maxWidth: '400px'}}>
+                <Button variant="text" href="/register">
                     Not registered yet? create an account
                 </Button>
             </Stack>
