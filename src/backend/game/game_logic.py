27d963195a122d01d39bb9dccca268c296881d3d
Erebos <boehm.pascal98@gmail.com>
2023-08-22 16:47:44 +0200

    refactorint

--- src/backend/game/game_logic.py
+++ src/backend/game/game_logic.py
@@ -1,44 +1,41 @@
 import random
-
-# obsolete
-""" def game(username):
-    running = True
-    has_won = False
-    guess_count = 0
-    random_integer = get_random_number()
-    while running:
-        # running game should be handled by front end
-        # outsource comparison of user input against random number
-        guess = get_user_input()
-        if guess == random_integer:
-            has_won = True
-            running = False
-            return username, guess_count, has_won
-        elif guess > random_integer:
-            return username, guess_count, has_won, "Try lower"
-        elif guess < random_integer:
-            return username, guess_count, has_won, "Try higher" """
+from enum import Enum
 
 # TODO Create GameManager class that has methods for number comparison and data persistence
 # TODO Remove other .py files
+# TODO return JSON objects
+# TODO rename file to class name
 
 
 class GameManager():
     def __init__(self) -> None:
         # set start score to 1000
         self.start_score = 1000
-
-    def calculate_highscore(self, guess_count):
-        # using "max" to compare the guess_count to 0 so if the score is negative (meaning the player took to many attempts) he just gets 0 points
-        self.highscore = max(self.start_score - 2**(guess_count - 1), 0)
-        return self.highscore
+        self.guess_count = 0
 
     def get_random_number(self):
         return random.randint(1, 100)
 
+    def get_guess_count(self):
+        return self.guess_count
+
     def compare_guess_to_random_integer(self, guess, random_integer):
+        class GuessStatus(Enum):
+            TOO_HIGH = 1
+            TOO_LOW = -1
+            CORRECT = 0
+
+        self.guess_count += 1
+
         if guess > random_integer:
-            pass
+            return GuessStatus.TOO_HIGH
+
         elif guess < random_integer:
-            pass
-        pass
+            return GuessStatus.TOO_LOW
+
+        return GuessStatus.CORRECT
+
+    def calculate_score(self, guess_count):
+        # using "max" to compare the guess_count to 0 so if the score is negative (meaning the player took to many attempts) he just gets 0 points
+        self.score = max(self.start_score - 2**(guess_count - 1), 0)
+        return self.score
--- src/backend/game/get_user_input.py
+++
@@ -1,3 +0,0 @@
-# TODO discuss how we implement the check if the user input an integer
-def get_user_input(user_input):
-    return int(user_input)
