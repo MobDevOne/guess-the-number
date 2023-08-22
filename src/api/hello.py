bc4b81e407d76e3d097071d0343526e13eb3bf67
Erebos <boehm.pascal98@gmail.com>
2023-08-22 17:32:23 +0200

    add TODOs

--- src/api/hello.py
+++ src/api/hello.py
@@ -1,5 +1,9 @@
 from flask import Flask
 
+# TODO rename file
+# TODO create functions to create user, get random_number, get_guess_count, calculate_score that correspond to the GameManager class methods
+
+
 app = Flask(__name__)
 
 
