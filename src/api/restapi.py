8d93fb59aec9e0b60186a0553dee11093b8f2cc6
Erebos <boehm.pascal98@gmail.com>
2023-08-23 09:29:48 +0200

    comments

--- src/api/restapi.py
+++ src/api/restapi.py
@@ -17,13 +17,15 @@ def create_new_user():
     user_data = request.args.get("register")
     user_manager.create_user(user_data[0], user_data[1])
 
+# no login method anymore?
+
 
 @app.route("/login")
 def login_user():
     login_data = request.args.get("login")
     # is this right? will this return the User instance? idk
     # and how would the score update work? can you send the user instance with a query string?
-    user_manager.log_in_user(login_data[0], login_data[1])
+    # user_manager.(login_data[0], login_data[1])
 
 
 if __name__ == "__main__":
