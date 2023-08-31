40dee46744243896f6ac5ac9b601f4031c89fe5c
Jean Pütz <93451392+JeanPtz@users.noreply.github.com>
2023-08-31 11:26:26 +0200

    changes api env to new address

--- src/frontend/src/apis/BaseUrl.ts
+++ src/frontend/src/apis/BaseUrl.ts
@@ -2,5 +2,5 @@ export function getBackendUrl(): string {
     if (process.env.NODE_ENV === 'development')
         return "http://localhost:8080"
     else
-        return "https://guess-api.sldw.de"
+        return "https://guess-api.jptz.de"
 }
