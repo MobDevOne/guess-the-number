1cffe512f9cb57194a5c22753d7c6cd3bfd87e68
Jean Pütz <93451392+JeanPtz@users.noreply.github.com>
2023-08-31 13:43:46 +0200

    chnaged base local url to actual local url

--- src/frontend/src/apis/BaseUrl.ts
+++ src/frontend/src/apis/BaseUrl.ts
@@ -1,6 +1,6 @@
 export function getBackendUrl(): string {
     if (process.env.NODE_ENV === 'development')
-        return "http://localhost:8080"
+        return "http://localhost:5000"
     else
         return "https://guess-api.jptz.de"
 }
